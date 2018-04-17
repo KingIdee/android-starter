9003ae05b4bf7a2f1bb3803f192f7d8546156650
Idorenyin Obong <idee4ril@gmail.com>
2018-04-17 03:15:40 +0100

    [+] create project

---
+++ app/src/main/java/com/kingidee/android/androidstarterkit/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.kingidee.android.androidstarterkit
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.android.androidstarter.R
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
